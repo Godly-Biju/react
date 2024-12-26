cd636fdc6fc5bbd038a5a4d2620cd49c7c56e40b
Godly-Biju <godlybiju0.com>
2024-12-26 19:04:49 +0530

    version 1

---
+++ src/MyCar.jsx
@@ -0,0 +1,10 @@
+// rcc - class creation
+import React, { Component } from 'react'
+
+export default class MyCar extends Component {
+  render() {
+    return (
+      <div>MyCar</div>
+    )
+  }
+}
